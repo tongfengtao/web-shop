f963ffe57ac0a9f8b8aebdc5a036881fb07f99b8
wangtao <wangtao@meloinfo.com>
2019-01-08 23:58:59 +0800

    秒嘀科技 注册送10元短信

---
+++ src/main/java/com/wangtao/web/shop/utils/httpApiDemo/common/Config.java
@@ -0,0 +1,27 @@
+package com.wangtao.web.shop.utils.httpApiDemo.common;
+
+/**
+ * 系统常量
+ */
+public class Config
+{
+	/**
+	 * url前半部分
+	 */
+	public static final String BASE_URL = "https://api.miaodiyun.com/20150822";
+
+	/**
+	 * 开发者注册后系统自动生成的账号，可在官网登录后查看
+	 */
+	public static final String ACCOUNT_SID = "f08f92cf892f4d9e8985278b1247ef25";
+
+	/**
+	 * 开发者注册后系统自动生成的TOKEN，可在官网登录后查看
+	 */
+	public static final String AUTH_TOKEN = "1cbb35ae5feb4ebbae4ce5abac1291ab";
+
+	/**
+	 * 响应数据类型, JSON或XML
+	 */
+	public static final String RESP_DATA_TYPE = "json";
+}
